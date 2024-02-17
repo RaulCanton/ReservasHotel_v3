cd0130170771e84550e96ccde71cd8c56df1cf74
Raul_Canton <raulcantong@hotmail.com>
2024-02-17 13:25:14 +0100

    Copio clases ejer anterior Módificado II

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -1,17 +1,18 @@
 package org.iesalandalus.programacion.reservashotel;
 
 
+import org.iesalandalus.programacion.reservashotel.Controlador.Controlador;
+import org.iesalandalus.programacion.reservashotel.Modelo.Modelo;
+import org.iesalandalus.programacion.reservashotel.vista.Vista;
+
 public class MainApp {
 
+    public static void main(String[] args)  {
 
-    public static void main(String[] args) {
-        System.out.println("Programa para la Gestión de Hoteles IES Al-Ándalus");
+        Modelo modelo= new Modelo();
+        Vista vista= new Vista();
+        Controlador controlador= new Controlador(modelo,vista);
+        controlador.comenzar();
 
     }
-
-
-
-
-
-
 }
